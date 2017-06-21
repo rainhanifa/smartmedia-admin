3a3c173932b46fd94f388273902c33f3d9a6bb7a
naayunir <naayunir@gmail.com>
2017-06-21 13:44:37 +0700

    update

--- application/controllers/Knowledgebase.php
+++ application/controllers/Knowledgebase.php
@@ -26,15 +26,24 @@
 		public function delete(){
 			// delete
 		}
-		public function category($action){
+		public function category($action="list"){
 			switch($action){
 				case 'add'		: //view category add
+					$this->load->view('template/header-admin.php');
+					$this->load->view('template/navbar-admin.php');
+					$this->load->view('knowledgebase/knowledgebase_create.php');
+					$this->load->view('template/footer-admin.php');
 								  break;
 				case 'update'	: //view category update
+					$this->load->view('template/header-admin.php');
+					$this->load->view('template/navbar-admin.php');
+					$this->load->view('knowledgebase/knowledgebase_update.php');
+					$this->load->view('template/footer-admin.php');
 								  break;
 				case 'delete'	: //action category delete
 								  break;
 				default 		: //view category list;
+									echo "list";
 								  break;
 			}
 		}
