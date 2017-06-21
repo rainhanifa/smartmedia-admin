7cdddbf3d091e5c246926ca1135b457fc8dd8ddb
Mahmuda17 <muthmm17@gmail.com>
2017-06-21 13:08:02 +0700

    update

--- application/controllers/Users.php
+++ application/controllers/Users.php
@@ -1,7 +1,7 @@
 <?php
 	defined('BASEPATH') OR exit('No direct script access allowed');
 
-	class User extends CI_Controller {
+	class Users extends CI_Controller {
 		public function index(){
 			$this->load->view('template/header-admin.php');
 			$this->load->view('template/navbar-admin.php');
